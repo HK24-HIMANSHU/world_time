be68b83261bf3aa963a01c696c85ac5426541bd4
Himanshu <itshimanshuk2002@gmail.com>
2021-03-03 21:34:52 +0530

    all the pages are being uploaded

---
+++ lib/pages/loading.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:world_time/services/world_time.dart';
+
+
+class Loading extends StatefulWidget {
+  @override
+  _LoadingState createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+
+
+ void setupWorldTime() async {
+    WorldTime instance = WorldTime(location: 'berlin',flag: 'germany.png',url:'Asia/Kolkata');
+    await instance.getTime();
+    Navigator.pushReplacementNamed(context, '/home',arguments: {
+      'location': instance.location,
+      'flag':instance.flag,
+      'time':instance.time,
+      'isDaytime': instance.isDaytime,
+    });
+
+
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupWorldTime();
+
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(50),
+        child: Text('loading'),
+      ),
+    );
+  }
+}
